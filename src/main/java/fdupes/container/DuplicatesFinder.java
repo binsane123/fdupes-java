755fbaf3b853c1ff1a996b55b4a7163be9b0a89a
Christophe Bismuth <christophe.bismuth@gmail.com>
2016-09-07 09:59:19 +0200

    Fixes #35 - Remove originals in a stream-oriented fashion

--- src/main/java/fdupes/container/DuplicatesFinder.java
+++ src/main/java/fdupes/container/DuplicatesFinder.java
@@ -29,8 +29,6 @@ import fdupes.io.PathEscapeFunction;
 import fdupes.md5.Md5SumHelper;
 
 import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
@@ -49,44 +47,30 @@ public class DuplicatesFinder {
         return elements.stream()
                        // index file metadata elements by file size
                        .collect(MultimapCollector.toMultimap(name("multimap", "by-size"), FileMetadata::getSize))
-
                        // get a stream of entries
                        .asMap().entrySet().stream()
-
                        // keep duplicates by size only
                        .filter(e -> e.getValue().size() > 1)
-
                        // flatten all duplicates by size
                        .flatMap(e -> e.getValue().stream())
 
                        // index file metadata elements by md5sum
                        .collect(MultimapCollector.toMultimap(name("multimap", "by-md5sum"), md5SumHelper::md5sum))
-
                        // get a stream of entries
                        .asMap().entrySet().stream()
-
                        // keep duplicates by md5sum only
                        .filter(e -> e.getValue().size() > 1)
-
                        // remove first element of each collection (i.e. original file)
-                       .peek(e -> sortAndRemoveFirst((List<FileMetadata>) e.getValue()))
-
+                       .map(KeepDuplicatesOnlyFunction.INSTANCE)
                        // flatten all duplicates by md5sum (i.e. all identified duplicates to remove)
                        .flatMap(e -> e.getValue().stream())
 
                        // extract absolute paths only
                        .map(FileMetadata::getAbsolutePath)
-
                        // normalize absolute paths
                        .map(PathEscapeFunction.INSTANCE)
-
                        // collect absolute paths
                        .collect(Collectors.toCollection(TreeSet::new));
-    }
-
-    private void sortAndRemoveFirst(final List<FileMetadata> value) {
-        Collections.sort(value, FileMetadataComparator.INSTANCE);
-        value.remove(0);
     }
 
 }
